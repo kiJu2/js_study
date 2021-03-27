3bd63451e59aaf3a0e2032c77a5ec30b9bb5eb11
kiJu2 <thdydehfdl12@gmail.com>
2021-03-27 12:00:08 +0900

    -

--- 객체/js.js
+++ 객체/js.js
@@ -6,15 +6,26 @@ function Person(name, gender){
   this.sayHello = function(){
     console.log('Hi~', this.name);
   }
-}
 
+  return 1
+}
 var person1 = new Person('lee', 'male');
 var person2 = new Person('kim', 'female');
 
-console.log(person1['name']);
-console.log(person1.gender)
-console.log(person1.three)
+console.log(person1)
+console.log(person2)
+// console.log(person1['name']);
+// console.log(person1.gender)
+// console.log(person1.three)
 
-
-
+// delete person1.name
+// console.log(person1)
 //프로퍼티 삭제부터
+
+// function modifyName(person){
+//   person.name = 'park';
+// }
+
+// console.log(person1)
+// modifyName(person1)
+// console.log(person1)
